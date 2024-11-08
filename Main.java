bf8becdf08fb9845c73b34e6a62f8140a46a11ec
RandyCHS <randy.macdonald@corvallis.k12.or.us>
2024-11-07 20:32:41 -0800

    Add initial implementation of sentiment analysis with review handling

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+class Main {
+  public static void main(String[] args) {
+    // Activity 1: Call the sentimentVal method in Review with a word like "terrible" and print out the result
+
+    double value = Review.sentimentVal("terrible");
+    System.out.println(value);
+    
+  }
+}
